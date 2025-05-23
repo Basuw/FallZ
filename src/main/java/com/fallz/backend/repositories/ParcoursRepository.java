f4e3cf2f40d631a04e8f0f9a2de39699901590b9
Bastien JACQUELIN <bastien.jacquelin@etu.uca.fr>
2025-05-23 13:45:16 +0000

    Broker 2

--- src/main/java/com/fallz/backend/repositories/ParcoursRepository.java
+++ src/main/java/com/fallz/backend/repositories/ParcoursRepository.java
@@ -1,16 +1,21 @@
 package com.fallz.backend.repositories;
 
-import java.util.List;
-import java.util.UUID;
-
+import com.fallz.backend.entities.Device;
+import com.fallz.backend.entities.Parcours;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.fallz.backend.entities.Device;
-import com.fallz.backend.entities.Parcours;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface ParcoursRepository extends JpaRepository<Parcours, UUID> {
-	
+    // Find active parcours (where endDate is null)
+    List<Parcours> findByEndDateIsNull();
+
+    // Find active parcours for a specific device
+    Optional<Parcours> findByDeviceIdAndEndDateIsNull(UUID deviceId);
+
 	List<Parcours> findByDevice(Device device);
 }
