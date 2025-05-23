285bf89fdf62d9f8b2ce607bd12f8447111bf3c7
yoboyer <Yoan.BOYER@etu.uca.fr>
2025-05-23 15:30:35 +0200

    fix Person

--- src/main/java/com/fallz/backend/entities/Person.java
+++ src/main/java/com/fallz/backend/entities/Person.java
@@ -1,5 +1,7 @@
 package com.fallz.backend.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
